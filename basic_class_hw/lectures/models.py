9ce29a17bfebdfad844a722ef4ac4a045095097e
minkyungbae <bmk0703@naver.com>
2025-01-19 22:14:19 +0900

    Update

--- basic_class_hw/lectures/models.py
+++ basic_class_hw/lectures/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Lecture(models.Model):
     lecture_id = models.AutoField(primary_key=True)  # 강의 ID (자동 증가)
-    name = models.CharField(max_length=255, null=False)  # 강의 이름
+    name = models.CharField(max_length=50, null=False)  # 강의 이름
     instructor_id = models.IntegerField(null=False)  # 강사 ID
     is_online = models.BooleanField(null=False)  # 온/오프라인 여부
     prerequisite = models.CharField(max_length=255, blank=True, null=True)  # 사전 강의
